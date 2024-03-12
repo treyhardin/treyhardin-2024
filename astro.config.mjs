28e7e79a1b91ece66510a9f535f309bf9f01268a
Trey Hardin <treyhardindesign@gmail.com>
2024-03-11 20:44:47 -0400

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["https://cdn.sanity.io"]
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
