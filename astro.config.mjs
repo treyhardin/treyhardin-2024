4614c588ed5e3a766af80db2d37e9243de37d15e
Trey Hardin <treyhardindesign@gmail.com>
2024-03-13 19:26:35 -0400

    Footer layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["https://cdn.sanity.io"]
   },
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [icon()]
 });
