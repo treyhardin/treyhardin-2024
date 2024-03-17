a1be8e1191225441c6bc011b65ad84e916e96cc8
Trey Hardin <treyhardindesign@gmail.com>
2024-03-17 14:45:59 -0400

    Video text animations

--- src/utils/gsap.jsx
+++ src/utils/gsap.jsx
@@ -9,3 +9,22 @@ export * from "gsap/ScrollTrigger";
 export * from "gsap/ScrollSmoother";
 
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother, ScrambleTextPlugin);
+
+export const mediaZoom = (element, timeline, timing) => {
+  timeline.from(element, {
+    scale: 1.4,
+    duration: 3
+  }, timing)
+}
+
+export const scrambleIn = ( element, timeline, timing) => {
+  timeline.to(element, {
+    autoAlpha: 1,
+    scrambleText: {
+      text: "{original}",
+      chars: "!@#$%^&*()TREY",
+      revealDelay: 0.6,
+      speed: 0.8,
+    },
+  }, timing)
+}
