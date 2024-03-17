0b42f0a655e23b33261e2e136a3088cf0d7845b0
Trey Hardin <treyhardindesign@gmail.com>
2024-03-16 20:01:48 -0400

    Scramble hero + header text

--- src/utils/gsap.jsx
+++ src/utils/gsap.jsx
@@ -2,9 +2,10 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 /* The following plugin is a Club GSAP perk */
 import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 
 export * from "gsap";
 export * from "gsap/ScrollTrigger";
 export * from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother, ScrambleTextPlugin);
